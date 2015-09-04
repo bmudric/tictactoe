c3dbdd6b002015670267346d9a9c10cf0d3ef66b
bmudric <bojan.mudric@gmail.com>
2015-09-04 16:31:11 +0200

    Implementing game endpoints

--- ZadatakKrizicKruzic/src/main/java/hr/bojan/zadatakkrizickruzic/web/controller/GameController.java
+++ ZadatakKrizicKruzic/src/main/java/hr/bojan/zadatakkrizickruzic/web/controller/GameController.java
@@ -1,12 +1,15 @@
 package hr.bojan.zadatakkrizickruzic.web.controller;
 
+import javax.servlet.http.HttpServletResponse;
+
 import hr.bojan.zadatakkrizickruzic.core.model.Game;
+import hr.bojan.zadatakkrizickruzic.core.model.exception.IllegalActionException;
 import hr.bojan.zadatakkrizickruzic.core.service.GameService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,8 +19,24 @@ public class GameController {
 	private GameService gameService;
 	
 	@RequestMapping(value="game/status", method=RequestMethod.GET)
-	public Game getStatus(@RequestParam("gameId") int gameId){
+	public Game getStatus(int gameId){
 		Game game = gameService.getGameStatus(gameId);
 		return game;
 	}
+	
+	@RequestMapping(value="game/new", method=RequestMethod.GET)
+	public int getNewGameBoth(String first, String second){
+		return this.gameService.createNewGame(first, second);
+	}
+	
+	@RequestMapping(value="game/play", method=RequestMethod.GET)
+	public Game getNewGameBoth(int gameId, short row, short column, HttpServletResponse response){
+		try{
+			return this.gameService.playGame(gameId, row, column);
+		}catch(IllegalActionException iae){
+			response.setStatus(HttpStatus.PRECONDITION_FAILED.value());
+			return null;
+		}
+	}
+	
 }
