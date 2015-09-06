b31fbc205b01ebfb2846a7dce640866321424cb2
bmudric <bojan.mudric@gmail.com>
2015-09-07 01:52:29 +0200

    Testing new game creation

---
+++ ZadatakKrizicKruzic/src/test/java/hr/bojan/zadatakkrizickruzic/core/service/GameServiceJunitTest.java
@@ -0,0 +1,63 @@
+package hr.bojan.zadatakkrizickruzic.core.service;
+
+import hr.bojan.zadatakkrizickruzic.ZadatakKrizicKruzicApplication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.Assert;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ZadatakKrizicKruzicApplication.class)
+public class GameServiceJunitTest {
+	
+	@Autowired
+	private GameService gameService;
+
+	@Test(expected=IllegalArgumentException.class)
+	public void testCreateNewGame_bothEmpty() {
+		this.gameService.createNewGame("", "");
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testCreateNewGame_bothNull() {
+		this.gameService.createNewGame(null, null);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testCreateNewGame_bothComputer() {
+		this.gameService.createNewGame("Computer", "Computer");
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testCreateNewGame_bothHuman() {
+		this.gameService.createNewGame("Human1", "Human2");
+	}
+	
+	@Test()
+	public void testCreateNewGame_humanFirst1() {
+		int gameId = this.gameService.createNewGame("HumanFirst1", "Computer");
+		Assert.isTrue(gameId > 0);
+	}
+	
+	@Test()
+	public void testCreateNewGame_humanFirst2() {
+		int gameId = this.gameService.createNewGame("HumanFirst2", "");
+		Assert.isTrue(gameId > 0);
+	}
+	
+	@Test()
+	public void testCreateNewGame_humanSecond1() {
+		int gameId = this.gameService.createNewGame("Computer", "HumanSecond1");
+		Assert.isTrue(gameId > 0);
+	}
+	
+	@Test()
+	public void testCreateNewGame_humanSecond2() {
+		int gameId = this.gameService.createNewGame("", "HumanSecond2");
+		Assert.isTrue(gameId > 0);
+	}
+
+}
