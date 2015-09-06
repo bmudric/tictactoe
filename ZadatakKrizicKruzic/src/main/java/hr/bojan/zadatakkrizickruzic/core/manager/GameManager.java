644c012026a849113bf736b6b34dff9ec50043af
bmudric <bojan.mudric@gmail.com>
2015-09-06 23:35:15 +0200

    Added creating a new game using a valid human player name

---
+++ ZadatakKrizicKruzic/src/main/java/hr/bojan/zadatakkrizickruzic/core/manager/GameManager.java
@@ -0,0 +1,14 @@
+package hr.bojan.zadatakkrizickruzic.core.manager;
+
+import hr.bojan.zadatakkrizickruzic.core.model.Game;
+
+public interface GameManager {
+
+	/**
+	 * Creates a new game and player if they don't exist
+	 * @param humanPlayer name of human player
+	 * @param computerGoesFirst if true computer is first player, else computer is second player
+	 * @return newly created game
+	 */
+	public Game createNewGame(String humanPlayer, boolean computerGoesFirst);
+}
---
+++ ZadatakKrizicKruzic/src/main/java/hr/bojan/zadatakkrizickruzic/core/manager/GameManagerImpl.java
@@ -0,0 +1,49 @@
+package hr.bojan.zadatakkrizickruzic.core.manager;
+
+import hr.bojan.zadatakkrizickruzic.core.dao.GameDao;
+import hr.bojan.zadatakkrizickruzic.core.dao.PlayerDao;
+import hr.bojan.zadatakkrizickruzic.core.model.Game;
+import hr.bojan.zadatakkrizickruzic.core.model.GameStatus;
+import hr.bojan.zadatakkrizickruzic.core.model.Player;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GameManagerImpl implements GameManager {
+	
+	@Autowired
+	private GameDao gameDao;
+	@Autowired
+	private PlayerDao playerDao;
+
+	@Override
+	public Game createNewGame(String humanPlayer, boolean computerGoesFirst) {
+		// create new human player if no such player already exists
+		Player human = this.playerDao.loadPlayerByName(humanPlayer);
+		if(human == null){
+			human = new Player(humanPlayer);
+			this.playerDao.insertNewPlayer(human);
+		}
+		
+		// create a new human vs. computer game
+		Game newGame = new Game();
+		if(computerGoesFirst){
+			newGame.setFirstPlayer(Player.COMPUTER);
+			newGame.setSecondPlayer(human);
+		}
+		else{
+			newGame.setFirstPlayer(human);
+			newGame.setSecondPlayer(Player.COMPUTER);
+		}
+		newGame.setStatus(GameStatus.IN_PROGRESS);
+		
+		boolean success = this.gameDao.insertNewGame(newGame);
+		if(!success){
+			throw new RuntimeException("Creating new game failed!");
+		}
+		
+		return newGame;
+	}
+
+}
