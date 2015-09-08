412b16d2c89209eabd69c2a53acd69c0d16524cb
bmudric <bojan.mudric@gmail.com>
2015-09-08 23:22:55 +0200

    Add possible endgame situations enum

---
+++ ZadatakKrizicKruzic/src/main/java/hr/bojan/zadatakkrizickruzic/core/model/EndgameStatus.java
@@ -0,0 +1,13 @@
+package hr.bojan.zadatakkrizickruzic.core.model;
+
+/**
+ * Possible endgame situations
+ * @author Bojan
+ */
+public enum EndgameStatus {
+
+	STILL_IN_PROGRESS,
+	DRAW,
+	X_WON,
+	O_WON;
+}
